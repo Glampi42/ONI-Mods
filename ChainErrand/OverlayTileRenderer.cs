6b7824dfe64da752dc9828ee85d15d1cfa0504fb
Glampi <glampimail42@gmail.com>
2026-06-15 22:22:44 +0200

    Update ChainErrand to v1.1.12.0: - fix for aquatic dlc game update

--- ChainErrand/OverlayTileRenderer.cs
+++ ChainErrand/OverlayTileRenderer.cs
@@ -22,7 +22,7 @@ namespace ChainErrand {
          if(!overlayRenderInfos.TryGetValue(key, out RenderInfo renderInfo))
          {
             renderInfo = new RenderInfo(World.Instance.blockTileRenderer, isReplacement ? (int)tileDef.ReplacementLayer : (int)tileDef.TileLayer,
-               Main.chainOverlay.targetLayer, tileDef, element);
+               Main.chainOverlay.targetLayer, tileDef, element, false);
             overlayRenderInfos.Add(key, renderInfo);
          }
          if(!renderInfo.occupiedCells.ContainsKey(cell))
